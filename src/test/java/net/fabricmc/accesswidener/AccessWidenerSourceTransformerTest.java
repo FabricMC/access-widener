4419982746724c956721e1329ad7175eee4c9539
Devan-Kerman <dev.sel20@gmail.com>
2021-10-15 22:20:59 -0500

    field & class tests

--- src/test/java/net/fabricmc/accesswidener/AccessWidenerSourceTransformerTest.java
+++ src/test/java/net/fabricmc/accesswidener/AccessWidenerSourceTransformerTest.java
@@ -1,7 +1,15 @@
 package net.fabricmc.accesswidener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -13,46 +21,10 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.ClassLoaderType
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 import net.fabricmc.accesswidener.javaparser.SourceAccessWidenerTransformer;
 
 class AccessWidenerSourceTransformerTest {
 	AccessWidener widener = new AccessWidener(true);
-	@Nested
-	class Classes {
-		@Test
-		public void testMakeAccessible() throws IOException {
-			widener.visitClass("test/PackagePrivateClass", AccessWidenerReader.AccessType.ACCESSIBLE, false);
-			CompilationUnit unit = applyTransformer("test.PackagePrivateClass");
-			TypeDeclaration<?> declaration = unit.getPrimaryType().orElseThrow();
-			assertEquals(NodeList.nodeList(Modifier.publicModifier()), declaration.getModifiers());
-		}
-
-		@Test
-		void testMakeExtendable() throws Exception {
-			widener.visitClass("test/PackagePrivateClass", AccessWidenerReader.AccessType.EXTENDABLE, false);
-			CompilationUnit testClass = applyTransformer("test.PackagePrivateClass");
-			TypeDeclaration<?> declaration = testClass.getPrimaryType().orElseThrow();
-			var modifiers = declaration.getModifiers();
-			assertTrue(modifiers.contains(Modifier.publicModifier()));
-			assertFalse(modifiers.contains(Modifier.finalModifier()));
-		}
-
-		@Test
-		void testMakeAccessibleAndExtendable() throws Exception {
-			widener.visitClass("test/FinalPackagePrivateClass", AccessWidenerReader.AccessType.ACCESSIBLE, false);
-			widener.visitClass("test/FinalPackagePrivateClass", AccessWidenerReader.AccessType.EXTENDABLE, false);
-			CompilationUnit testClass = applyTransformer("test.FinalPackagePrivateClass");
-			TypeDeclaration<?> declaration = testClass.getPrimaryType().orElseThrow();
-			var modifiers = declaration.getModifiers();
-			assertTrue(modifiers.contains(Modifier.publicModifier()));
-			assertFalse(modifiers.contains(Modifier.finalModifier()));
-		}
-
-	}
-
 
 	private CompilationUnit applyTransformer(String rss) throws IOException {
 		JavaParser parser = new JavaParser();
@@ -62,5 +34,94 @@ class AccessWidenerSourceTransformerTest {
 		SourceAccessWidenerTransformer transformer = new SourceAccessWidenerTransformer(this.widener);
 		transformer.transform(unit);
 		return unit;
+	}
+
+	@Nested
+	class Classes {
+		@Test
+		public void testMakeAccessible() throws IOException {
+			widener.visitClass("test/PackagePrivateClass", AccessWidenerReader.AccessType.ACCESSIBLE, false);
+			CompilationUnit unit = applyTransformer("test.PackagePrivateClass");
+			TypeDeclaration<?> declaration = unit.getPrimaryType().orElseThrow();
+			assertThat(declaration.getModifiers()).contains(Modifier.publicModifier());
+		}
+
+		@Test
+		void testMakeExtendable() throws Exception {
+			widener.visitClass("test/PackagePrivateClass", AccessWidenerReader.AccessType.EXTENDABLE, false);
+			CompilationUnit testClass = applyTransformer("test.PackagePrivateClass");
+			TypeDeclaration<?> declaration = testClass.getPrimaryType().orElseThrow();
+			assertThat(declaration.getModifiers()).contains(Modifier.publicModifier()).doesNotContain(Modifier.finalModifier());
+		}
+
+		@Test
+		void testMakeAccessibleAndExtendable() throws Exception {
+			widener.visitClass("test/FinalPackagePrivateClass", AccessWidenerReader.AccessType.ACCESSIBLE, false);
+			widener.visitClass("test/FinalPackagePrivateClass", AccessWidenerReader.AccessType.EXTENDABLE, false);
+			CompilationUnit testClass = applyTransformer("test.FinalPackagePrivateClass");
+			TypeDeclaration<?> declaration = testClass.getPrimaryType().orElseThrow();
+			var modifiers = declaration.getModifiers();
+			assertThat(modifiers).contains(Modifier.publicModifier()).doesNotContain(Modifier.finalModifier());
+		}
+
+		@Test
+		void testMakeInnerClassAccessible() throws Exception {
+			widener.visitClass("test/PrivateInnerClass$Inner", AccessWidenerReader.AccessType.ACCESSIBLE, false);
+			CompilationUnit testClass = applyTransformer("test.PrivateInnerClass");
+			TypeDeclaration<?> inner = testClass.getPrimaryType()
+					.map(t -> t.findAll(TypeDeclaration.class, p -> p != t))
+					.map(List::stream)
+					.flatMap(Stream::findAny)
+					.orElseThrow();
+			assertThat(inner.getModifiers()).contains(Modifier.publicModifier());
+		}
+	}
+
+	@Nested
+	class Fields {
+		@Test
+		void testMakeAccessible() throws Exception {
+			widener.visitField("test/FieldTests", "privateFinalIntField", "I", AccessWidenerReader.AccessType.ACCESSIBLE, false);
+			CompilationUnit unit = applyTransformer("test.FieldTests");
+			TypeDeclaration<?> testClass = unit.getPrimaryType().orElseThrow();
+
+			assertThat(List.of(Modifier.publicModifier(), Modifier.finalModifier()))
+					.isEqualTo(testClass.getModifiers())
+					.isEqualTo(testClass.getFieldByName("privateFinalIntField").orElseThrow().getModifiers());
+		}
+
+		@Test
+		void testMakeMutable() throws Exception {
+			widener.visitField("test/FieldTests", "privateFinalIntField", "I", AccessWidenerReader.AccessType.MUTABLE, false);
+			CompilationUnit unit = applyTransformer("test.FieldTests");
+			TypeDeclaration<?> testClass = unit.getPrimaryType().orElseThrow();
+
+			// making the field mutable does not affect the containing class
+			assertThat(List.of(Modifier.finalModifier())).isEqualTo(testClass.getModifiers());
+			assertThat(List.of(Modifier.privateModifier())).isEqualTo(testClass.getFieldByName("privateFinalIntField").orElseThrow().getModifiers());
+		}
+
+		@Test
+		void testMakeMutableAndAccessible() throws Exception {
+			widener.visitField("test/FieldTests", "privateFinalIntField", "I", AccessWidenerReader.AccessType.MUTABLE, false);
+			widener.visitField("test/FieldTests", "privateFinalIntField", "I", AccessWidenerReader.AccessType.ACCESSIBLE, false);
+			CompilationUnit unit = applyTransformer("test.FieldTests");
+			TypeDeclaration<?> testClass = unit.getPrimaryType().orElseThrow();
+
+			// Making the field accessible and mutable affects the class visibility
+			assertThat(List.of(Modifier.publicModifier(), Modifier.finalModifier()))
+					.isEqualTo(testClass.getModifiers());
+			assertThat(List.of(Modifier.publicModifier())).isEqualTo(testClass.getFieldByName("privateFinalIntField").orElseThrow().getModifiers());
+		}
+
+		@Test
+		void testDontMakeInterfaceMutable() throws Exception {
+			widener.visitField("test/InterfaceTests", "staticFinalIntField", "I", AccessWidenerReader.AccessType.MUTABLE, false);
+			CompilationUnit unit = applyTransformer("test.InterfaceTests");
+			TypeDeclaration<?> testClass = unit.getPrimaryType().orElseThrow();
+
+			assertThat(List.of(Modifier.publicModifier(), Modifier.finalModifier(), Modifier.staticModifier()))
+					.isEqualTo((testClass.getFieldByName("staticFinalIntField").orElseThrow().getModifiers()));
+		}
 	}
 }
